0a6ce0873c8c73f81bb8abe31e2969db4029ff23
sahilsummit <sjaura@thesummitgrp.com>
2024-06-22 14:21:05 +0530

    fresh start

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+"use strict";
+const greet = (name) => {
+    console.log(`Hello, ${name}!`);
+};
+greet("sahil");
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+
+const greet = (name: string) => {
+  console.log(`Hello, ${name}!`);
+};
+greet("sahil");
